7ae828a53aa9da0ad56f70aa345d9cc6a27f68fd
s278613 <s278613@P2025170>
2020-06-10 01:02:36 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploadss"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://uvvei37x1f.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_mcHOqdzYm",
+    APP_CLIENT_ID: "6tgs5no3gr71hugbesap9delku",
+    IDENTITY_POOL_ID: "us-east-1:d2d2e4f0-e4f4-4144-b9d0-945456ad6df1"
+  }
+};
